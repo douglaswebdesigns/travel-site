9fd4039aedbe4b07e40e9558eedf8e704e132006
douglaswebdesigns <paul@douglaswebdesigns.com>
2016-09-23 11:09:49 -0400

    updated folder structure

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import MobileMenu from "./modules/MobileMenu';
-=======
-import MobileMenu from '../modules/MobileMenu';
-<<<<<<< HEAD
->>>>>>> MobileMenu
-=======
+import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from '../modules/RevealOnScroll';
+import StickyHeader from '../modules/StickyHeader';
 import $ from 'jquery';
->>>>>>> reveal-on-scroll
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
